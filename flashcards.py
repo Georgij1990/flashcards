67d68aeecdec6c485518533be3a66d0bec98d3c2
Georgij Kocsarjan <georgij.kocsarjan@gmail.com>
2024-02-22 20:51:15 +0100

    Pass Stage 3

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,16 @@
-print('Card:')
-term = input()
-definition = input()
-answer = input()
-if answer == definition:
-    print('Your answer is right!')
-else:
-    print('Your answer is wrong...')
+print('Input the number of cards:')
+num_of_flashcards = int(input())
+flashcards = {}
+for i in range(num_of_flashcards):
+    print(f'The term for card {i + 1}')
+    term = input()
+    print(f'The definition for card {i + 1}:')
+    definition = input()
+    flashcards[term] = definition
+for k, v in flashcards.items():
+    print(f'Print the definition of "{k}":')
+    guess_definition = input()
+    if flashcards[k] == guess_definition:
+        print('Correct!')
+    else:
+        print(f'Wrong. The right answer is "{flashcards[k]}".')
